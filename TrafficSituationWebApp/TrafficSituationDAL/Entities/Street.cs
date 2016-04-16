ae76d9de096d7eb2e85be886341bb08692f34e4f
Дубовик Ярослав < >
2016-04-16 12:28:17 +0300

    DAL and BLL have been created Signed-off-by: Dub4ik

---
+++ TrafficSituationWebApp/TrafficSituationDAL/Entities/Street.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSituationDAL.Entities
+{
+    public class Street:Entity
+    {
+        public string Name { get; set; }
+        public string Geolocation { get; set; }
+        public int CityId { get; set; }
+        public List<Accident> Accidents { get; set; }
+        public List<ManuallyGeneratedData> ManuallyGeneratedDataList { get; set; }
+        public List<AutomaticallyGeneratedData> AutomaticallyGeneratedDataList { get; set; }
+        public List<TrafficSituationReport> TrafficSituationReports{ get; set; }    
+    }
+}
